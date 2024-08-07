31c10e12ce5ff1c55bc7747d8b57cfea1eed86e7
Peter <112364552+p-Lennart@users.noreply.github.com>
2024-08-07 00:43:01 -0400

    Render MP4 Videos

--- server/channelMessageFiles.mjs
+++ server/channelMessageFiles.mjs
@@ -10,13 +10,12 @@ export function messageFilesUI(ARCHIVE_DATA, message) {
     const result = [];
 
     for (const file of message.files) {
+        if (!file.mimetype) {
+            file.mimetype = 'NONE/NONE';
+        }
+
         if (!file.permalink) {
             console.error('File missing permalink ' + file);
-            continue;
-        }
-        
-        if (!file.mimetype) {
-            console.log('Generic file handling ' + file);
             continue;
         }
 
@@ -24,6 +23,19 @@ export function messageFilesUI(ARCHIVE_DATA, message) {
             result.push(imageUI(ARCHIVE_DATA, file) )
             continue;
         }
+
+        if (file.mimetype.startsWith('image/')) {
+            result.push(imageUI(ARCHIVE_DATA, file) )
+            continue;
+        }
+
+        if (file.mimetype.startsWith('video/')) {
+            result.push(videoUI(ARCHIVE_DATA, file) )
+            continue;
+        }
+
+        console.log('Generic file handling ' + file);
+            
     }
 
     return result;
@@ -33,4 +45,12 @@ function imageUI(ARCHIVE_DATA, imageFile) {
     let source = imageFile.url_private;
     // let source = localPathFromFile(ARCHIVE_DATA, imageFile); 
     return jml('img', { src: source });
+}
+
+function videoUI(ARCHIVE_DATA, imageFile) {
+    let source = imageFile.mp4;
+    // let source = localPathFromFile(ARCHIVE_DATA, imageFile); 
+    return jml('video', { controls: true }, [
+        jml('source', { src: source, type: 'video/mp4'}),
+    ]);
 }
